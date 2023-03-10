7de0bbf3d8311cae0c68acdb7342eab124713880
Markaz Markazov <sageokweda023@gmail.com>
2023-03-10 15:24:26 +0500

    test
    test uchun file

---
+++ masin2.py
@@ -0,0 +1,80 @@
+
+# cars = ["lacetti", "nexia", "toyota", "nexia", "audi", "malibu", "nexia", "lacetti"]
+# car = "lacetti"
+# car2 = "nexia"
+# while car in cars:
+#     cars.remove(car)
+#     cars.remove(car2)
+    
+# print(cars)
+
+
+talabalar = ["hasan", "husan", "olim", "botir"]
+baholangan_talabalar = {}
+while talabalar:
+    talaba = talabalar.pop()
+    baho = input(f"{talaba.title()}ning bahosini kiriting: ")
+    print(f"{talaba.title()} baholandi")
+    baholangan_talabalar[talaba] = int(baho)
+
+
+
+# cars = ["lacetti", "nexia", "toyota", "nexia", "audi", "malibu", "nexia", "lacetti"]
+# car = "lacetti"
+# car2 = "nexia"
+# while car in cars:
+#     cars.remove(car)
+#     cars.remove(car2)
+    
+# print(cars)
+
+
+talabalar = ["hasan", "husan", "olim", "botir"]
+baholangan_talabalar = {}
+while talabalar:
+    talaba = talabalar.pop()
+    baho = input(f"{talaba.title()}ning bahosini kiriting: ")
+    print(f"{talaba.title()} baholandi")
+    baholangan_talabalar[talaba] = int(baho)
+
+
+
+# cars = ["lacetti", "nexia", "toyota", "nexia", "audi", "malibu", "nexia", "lacetti"]
+# car = "lacetti"
+# car2 = "nexia"
+# while car in cars:
+#     cars.remove(car)
+#     cars.remove(car2)
+    
+# print(cars)
+
+
+talabalar = ["hasan", "husan", "olim", "botir"]
+baholangan_talabalar = {}
+while talabalar:
+    talaba = talabalar.pop()
+    baho = input(f"{talaba.title()}ning bahosini kiriting: ")
+    print(f"{talaba.title()} baholandi")
+    baholangan_talabalar[talaba] = int(baho)
+
+
+
+# cars = ["lacetti", "nexia", "toyota", "nexia", "audi", "malibu", "nexia", "lacetti"]
+# car = "lacetti"
+# car2 = "nexia"
+# while car in cars:
+#     cars.remove(car)
+#     cars.remove(car2)
+    
+# print(cars)
+
+
+talabalar = ["hasan", "husan", "olim", "botir"]
+baholangan_talabalar = {}
+while talabalar:
+    talaba = talabalar.pop()
+    baho = input(f"{talaba.title()}ning bahosini kiriting: ")
+    print(f"{talaba.title()} baholandi")
+    baholangan_talabalar[talaba] = int(baho)
+
+
